34e913c7cf5a8590621e8361575abeeee477012e
EdIvarsson413 <EadIvarsson413>
2022-11-30 20:58:02 -0600

    FrontEnd hecho

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import './app.css'
+
+function App() {
+  return (
+    <><header>
+        <h1>Separador de textiles</h1>
+        <button>Descargar</button>
+    </header>
+      
+    
+    <table>
+      <tr>
+        <th>Color </th>
+        <th> Peso</th>
+        <th>Cantidad</th>
+      </tr>
+      <tr>
+        <td>Azul</td>
+        <td>100gr</td>
+        <td>1</td>
+      </tr>
+      <tr>
+        <td>Rojo</td>
+        <td>200gr</td>
+        <td>3</td>
+      </tr>
+      <tr>
+        <td>Verde</td>
+        <td>400gr</td>
+        <td>3</td>
+      </tr>
+    </table>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Tabla.jsx
@@ -0,0 +1,6 @@
+
+export const Tabla = () => {
+  return (
+    <div>Tabla</div>
+  )
+}
